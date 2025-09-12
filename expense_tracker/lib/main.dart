21b6c4c65c7ba72258dc6fd90a4d5c88f41dd06a
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-09-11 17:29:16 -0700

    formatting

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -3,40 +3,45 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/Expenses.dart';
 
 ColorScheme colourScheme = ColorScheme.fromSeed(
-          seedColor: Color.fromARGB(255, 59, 1, 1),
-          brightness: Brightness.dark,
-        );
+  seedColor: Color.fromARGB(255, 59, 1, 1),
+  brightness: Brightness.dark,
+);
 
 ThemeData lightTheme = ThemeData.light().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color.fromARGB(255, 59, 1, 1),
-          brightness: Brightness.light,
-        ),
-      );
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Color.fromARGB(255, 59, 1, 1),
+    brightness: Brightness.light,
+  ),
+);
 
 ThemeData darkTheme = ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Color.fromARGB(255, 43, 38, 71), // This overrides colour scheme
-        // Better to do the entire scheme instead of individual colours
-        colorScheme: colourScheme,
-        // Or do this to make a specific theme for a widget
-        appBarTheme: AppBarTheme().copyWith(
-          centerTitle: false,
-          backgroundColor: colourScheme.onPrimary
-        ),
-        // Stylise the cards
-        cardTheme: CardThemeData().copyWith(
-          color: colourScheme.primaryContainer,
-          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        // Can also target text types
-        textTheme: ThemeData.dark().textTheme.copyWith(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 28,
-            color: colourScheme.onPrimaryContainer,
-          ),
-        ),
-      );
+  scaffoldBackgroundColor: Color.fromARGB(
+    255,
+    43,
+    38,
+    71,
+  ), // This overrides colour scheme
+  // Better to do the entire scheme instead of individual colours
+  colorScheme: colourScheme,
+  // Or do this to make a specific theme for a widget
+  appBarTheme: AppBarTheme().copyWith(
+    centerTitle: false,
+    backgroundColor: colourScheme.onPrimary,
+  ),
+  // Stylise the cards
+  cardTheme: CardThemeData().copyWith(
+    color: colourScheme.primaryContainer,
+    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+  ),
+  // Can also target text types
+  textTheme: ThemeData.dark().textTheme.copyWith(
+    titleLarge: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 28,
+      color: colourScheme.onPrimaryContainer,
+    ),
+  ),
+);
 
 void main() {
   runApp(
--- expense_tracker/lib/models/Expense.dart
+++ expense_tracker/lib/models/Expense.dart
@@ -5,18 +5,13 @@ import 'package:intl/intl.dart';
 final uuidGen = Uuid();
 final dateFormatter = DateFormat.yMMMd();
 
-enum Category {
-    FOOD,
-    TRAVEL,
-    LEISURE,
-    WORK
-}
+enum Category { FOOD, TRAVEL, LEISURE, WORK }
 
 const categoryIcons = {
   Category.FOOD: Icons.lunch_dining,
   Category.LEISURE: Icons.movie,
   Category.TRAVEL: Icons.flight,
-  Category.WORK: Icons.work
+  Category.WORK: Icons.work,
 };
 
 class Expense {
@@ -27,14 +22,38 @@ class Expense {
   final Category category;
 
   // Can initialize properties with this following colon syntax
-  Expense({required this.title, required this.amount, required this.date, required this.category})
-    : id = uuidGen.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuidGen.v4();
 
-  IconData getIcon(){
-    return  categoryIcons[category] ?? Icons.error;
+  IconData getIcon() {
+    return categoryIcons[category] ?? Icons.error;
   }
 
-  String getDate(){
+  String getDate() {
     return dateFormatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  final Category category;
+  final List<Expense> expenses;
+
+  ExpenseBucket({required this.category, required this.expenses});
+
+  // Example of alternative constructor
+  // Use like:
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses.where((ex) => ex.category == category).toList();
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final ex in expenses) {
+      sum += ex.amount;
+    }
+    return sum;
   }
 }
