ef7224b41744e7a2141eb7711cab817f75629742
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-09-24 18:55:05 -0700

    using Hero widget to animate. wow!

--- meals_app/lib/widgets/meal_item.dart
+++ meals_app/lib/widgets/meal_item.dart
@@ -31,14 +31,17 @@ class MealItem extends StatelessWidget {
         child: Stack(
           // First child is the base, next child is on top of it
           children: [
-            FadeInImage(
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
-              placeholder: MemoryImage(
-                kTransparentImage,
-              ), // From transparent_image package
-              image: NetworkImage(meal.imageUrl),
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+                placeholder: MemoryImage(
+                  kTransparentImage,
+                ), // From transparent_image package
+                image: NetworkImage(meal.imageUrl),
+              ),
             ),
             Positioned(
               bottom: 0,
