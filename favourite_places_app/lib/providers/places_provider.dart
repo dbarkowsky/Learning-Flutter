30cd863671f188db1e2cb1421c251b5e2bf4168a
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2025-10-07 16:45:45 -0700

    save image to local file storage

--- favourite_places_app/lib/providers/places_provider.dart
+++ favourite_places_app/lib/providers/places_provider.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favourite_places_app/models/place.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class PlacesProvider extends Notifier<List<Place>> {
   final List<Place> _places = [];
@@ -13,8 +17,14 @@ class PlacesProvider extends Notifier<List<Place>> {
     return _places;
   }
 
-  void addPlace(Place p){
-    _places.add(p);
+  void addPlace(Place p) async {
+    // Save the image to the device
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(p.image.path);
+    File imageFile = await p.image.copy('${appDir.path}/$fileName');
+
+    Place placeWithLocalImage = Place(title: p.title, location: p.location, image: imageFile);
+    _places.add(placeWithLocalImage);
   }
 
   void removePlace(Place p){
